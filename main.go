ca740ac2bd277603e5f43df5670fda47df55e6f3
mikolaszko <mikosodz.lote@gmail.com>
2022-12-13 19:49:32 +0100

    Removed slcom command because it was useless

--- main.go
+++ main.go
@@ -16,17 +16,9 @@ var (
 		Short: "An example cobra program",
 		Long:  "A longer example cobra program",
 	}
-	SuslinkscomCmd = &cobra.Command{
-		Use:   "slcom [url to scrape]",
-		Short: "Scrapes provided https://url for hrefs",
-		Args:  cobra.MaximumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			helpers.ScrapeLinksCom(args)
-		},
-	}
 	SuslinksCmd = &cobra.Command{
 		Use:   "sl [url to scrape, domain]",
-		Short: "Scrapes provided https://url (thats not .com) for hrefs",
+		Short: "Scrapes provided https://url for hrefs",
 		Args:  cobra.MaximumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			helpers.ScrapeLinks(args)
@@ -46,7 +38,6 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlat", "p", false, "a persistant root flag")
 	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
 	// timesCmd.Flags().IntVarP(&times, "times", "t", 1, "number of times to echo to stdout")
-	rootCmd.AddCommand(SuslinkscomCmd)
 	rootCmd.AddCommand(SuslinksCmd)
 	rootCmd.AddCommand(SusheadlinesCmd)	
 }
