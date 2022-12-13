0c061e1b333466d3e23dea1619699b386de4f130
mikolaszko <mikosodz.lote@gmail.com>
2022-12-13 23:07:42 +0100

    Tried it on gitlab, its not working there either

--- helpers/ScrapeContributors.go
+++ helpers/ScrapeContributors.go
@@ -2,7 +2,6 @@ package helpers
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -10,24 +9,18 @@ import (
 func ScrapeContributors(args []string) {
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("github.com"),
+		colly.AllowedDomains("gitlab.com"),
 	)
 
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r.StatusCode, "\nError:", err)
 	})
 
-	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
-		if e.Attr("class") == "avatar avatar-user" {
-		link := e.Attr("src")
+	c.OnHTML("span", func(e *colly.HTMLElement) {
+		link := e.Text
 		fmt.Println(link)
-		}
 	})
-	builder := strings.Builder{}
-        builder.WriteString(args[0])
- 	builder.WriteString("/graphs/contributors")
-	fmt.Println(builder.String())
 
-	c.Visit(builder.String())
+	c.Visit(args[0])
 
 }
